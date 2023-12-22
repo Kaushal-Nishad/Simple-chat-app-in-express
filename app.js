2cbff01efe56ffb72a261678da1c08f54b13de1d
Kaushal Nishad <kaushalnishad212@gmail.com>
2023-12-22 20:18:52 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files (e.g., HTML, CSS, JS)
+app.use(express.static(__dirname + '/public'));
+
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for chat messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
